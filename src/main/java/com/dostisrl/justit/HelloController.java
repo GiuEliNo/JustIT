53cefd7b3ae04cc13b3919c24f42067366eb55cd
giuelino <rgiulio59@gmail.com>
2024-11-08 16:49:53 +0100

    first commit

---
+++ src/main/java/com/dostisrl/justit/HelloController.java
@@ -0,0 +1,14 @@
+package com.dostisrl.justit;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
