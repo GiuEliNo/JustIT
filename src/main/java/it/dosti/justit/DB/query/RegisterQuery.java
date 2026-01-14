df86410ff315152f509eea7df59035da95b339ed
giuelino <rgiulio59@gmail.com>
2026-01-14 16:26:34 +0100

    sistemato altre query in preparazione per register

--- src/main/java/it/dosti/justit/DB/query/RegisterQuery.java
+++ src/main/java/it/dosti/justit/DB/query/RegisterQuery.java
@@ -24,20 +24,48 @@ public class RegisterQuery {
     }
 
 
-    public static boolean RegisterTechnician(Connection conn, String username, String password, String email, String Shop) throws SQLException {
+    public static boolean RegisterTechnician(Connection conn, String username, String password, String name, String email, String Shop) throws SQLException {
         String sql;
-        sql = "INSERT INTO TECHNICIAN (username, password, email, name, shop_id) " +
+        sql = "INSERT INTO TECHNICIAN (username, password, email, name, shop) " +
                 "VALUES (?, ?, ?, ?, (SELECT id FROM Shop WHERE name = ?))";
         PreparedStatement pstmt = conn.prepareStatement(sql);
         pstmt.setString(1, username);
         pstmt.setString(2, password);
         pstmt.setString(3, email);
-        pstmt.setString(4, Shop);
+        pstmt.setString(4, name);
         pstmt.setString(5, Shop);
         try{
             pstmt.executeUpdate(sql);
             return true;
         }catch(SQLException e){
+            return false;
+        }
+    }
+
+    public static boolean RegisterShop(Connection conn, String name, String address, String phone, String email, String descriptions, String image, String openingHours, boolean homeAssistance) throws SQLException {
+
+        String sql;
+
+
+        sql="INSERT INTO SHOP (name,address,phone,email,descriptions,image,openingHours,homeAssistance) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        pstmt.setString(1, name);
+        pstmt.setString(2, address);
+        pstmt.setString(3, phone);
+        pstmt.setString(4, email);
+        pstmt.setString(5, descriptions);
+        pstmt.setString(6, image);
+        pstmt.setString(7, openingHours);
+        pstmt.setBoolean(8,homeAssistance);
+
+        try{
+            pstmt.executeUpdate(sql);
+            return true;
+        }
+        catch(SQLException e){
             return false;
         }
     }
