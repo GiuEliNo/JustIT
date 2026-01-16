a9a4c9b6315516f8bc4c8ecf24afddc82ea0d648
valerio <vle.mazza@gmail.com>
2026-01-16 12:36:56 +0100

    spostato MainApp.java dentro GUIMode.java

--- src/main/java/it/dosti/justit/GUIMode.java
+++ src/main/java/it/dosti/justit/GUIMode.java
@@ -1,16 +1,41 @@
 package it.dosti.justit;
 
+import it.dosti.justit.ui.navigation.NavigationService;
+import it.dosti.justit.ui.navigation.Screen;
+import it.dosti.justit.ui.navigation.ViewFactory;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.sql.SQLException;
 
-public class GUIMode extends BaseAppMode {
+public class GUIMode extends BaseAppMode implements AppMode{
     @Override
     public void start(String[] args) {
         try {
             connectToDB();
-            Application.launch(MainApp.class, args);
+            Application.launch(GUIApplication.class, args);
         } catch (SQLException ignored) {
+        }
+    }
+
+    public static class GUIApplication extends Application {
+        @Override
+        public void start(Stage stage) {
+            ViewFactory viewFactory = new ViewFactory();
+            StackPane root = new StackPane();
+            NavigationService.init(root, viewFactory);
+
+            Scene scene = new Scene(root, 900, 600);
+            stage.setScene(scene);
+            stage.setTitle("JustIT");
+            stage.setResizable(false);
+            stage.show();
+            Platform.runLater(stage::centerOnScreen);
+
+            NavigationService.navigateToRoot(Screen.LAUNCHER);
         }
     }
 }
