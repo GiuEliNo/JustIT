5dfda8c79d5446f619d6f9520ce38a5756951fc0
giuelino <rgiulio59@gmail.com>
2026-02-06 19:51:41 +0100

    Corretta la query per cambiare la password del tecnico

--- src/main/java/it/dosti/justit/db/query/TechnicianQuery.java
+++ src/main/java/it/dosti/justit/db/query/TechnicianQuery.java
@@ -7,5 +7,5 @@ public class TechnicianQuery {
     public static final String SELECT_BY_USERNAME = "SELECT id, username, name, email, shop FROM Technician WHERE username = ?";
     public static final String UPDATE_NAME = "UPDATE Technician SET name = ? WHERE username = ?";
     public static final String UPDATE_EMAIL = "UPDATE Technician SET email = ? WHERE username = ?";
-    public static final String UPDATE_PASSWORD = "UPDATE Technician SET name = ? WHERE username = ? AND password = ?";
+    public static final String UPDATE_PASSWORD = "UPDATE Technician SET password = ? WHERE username = ? AND password = ?";
 }
