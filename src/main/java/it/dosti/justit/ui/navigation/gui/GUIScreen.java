481f8dd32e2bf0e4d9a924e1bebb830caa4f9a0c
valerio <vle.mazza@gmail.com>
2026-02-06 16:47:15 +0100

    migliorato i nomi dei fxml

--- src/main/java/it/dosti/justit/ui/navigation/gui/GUIScreen.java
+++ src/main/java/it/dosti/justit/ui/navigation/gui/GUIScreen.java
@@ -3,24 +3,22 @@ package it.dosti.justit.ui.navigation.gui;
 public enum GUIScreen {
 
     LAUNCHER("/login.fxml"),
-    SIDEBAR_SEARCH_LIST("/sidebarListPage.fxml"),
-    MAIN_USER("/mainUser.fxml"),
-    MAIN_TECH("/mainTech.fxml"),
+    SIDEBAR_SEARCH_LIST("/searchListShop.fxml"),
     PAGE_SHOP("/pageShopUser.fxml"),
-    BOOKING_PAGE("/bookingPage.fxml"),
+    BOOKING_PAGE("/bookingPageUser.fxml"),
     REVIEWS_BOX("/reviewsBox.fxml"),
     REGISTER_VIEW("/registerUser.fxml"),
     REGISTERTEC_VIEW("/registerTech.fxml"),
     TOPBAR("/topBar.fxml"),
     REGISTER_SHOP("/registerShop.fxml"),
     ACCOUNT_PAGE("/accountPage.fxml"),
-    MESSAGES("/messages.fxml"),
+    MESSAGES("/notificationCenterUser.fxml"),
     PAYMENTS("/payments.fxml"),
     PAGE_SHOP_TECH("/pageShopTech.fxml"),
-    BOOKINGS("/bookings.fxml"),
-    BOOKING_PAGE_TECH("/bookingPageTech.fxml"),
+    BOOKINGS("/bookingsListUser.fxml"),
+    BOOKING_PAGE_TECH("/bookingsListTech.fxml"),
     SIDEBAR_TECH_LIST("/sidebarListTechPage.fxml"),
-    MESSAGES_TECH("/messagesTech.fxml");
+    MESSAGES_TECH("/notificationCenterTech.fxml");
 
     private final String fxmlPath;
 
