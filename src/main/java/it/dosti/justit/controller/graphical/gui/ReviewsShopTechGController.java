a985867fcdc8510eae0f8dfa79338d22a9a60766
valerio <vle.mazza@gmail.com>
2026-02-07 17:15:49 +0100

    aggiunta review list epr il tecnico

--- src/main/java/it/dosti/justit/controller/graphical/gui/ReviewsShopTechGController.java
+++ src/main/java/it/dosti/justit/controller/graphical/gui/ReviewsShopTechGController.java
@@ -1,6 +1,8 @@
 package it.dosti.justit.controller.graphical.gui;
 
-import it.dosti.justit.model.review.Review;
+import it.dosti.justit.bean.ReviewBean;
+import it.dosti.justit.controller.app.ReviewController;
+import it.dosti.justit.view.gui.ReviewListCell;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -8,8 +10,21 @@ import javafx.scene.control.ListView;
 public class ReviewsShopTechGController extends BaseGController {
 
     @FXML
-    private ListView<Review> reviewsList;
+    private ListView<ReviewBean> listReview;
 
     @FXML
     private Button answerReviewButton;
+
+    private ReviewController appController;
+
+    @FXML
+    public void initialize() {
+       appController = new ReviewController();
+        listReview.setCellFactory(lr -> new ReviewListCell());
+        this.updateReviewList();
+    }
+
+    public void updateReviewList() {
+        listReview.getItems().setAll(appController.getReviews());
+    }
 }
