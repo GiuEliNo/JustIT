aa94875e74867914a9d366f15096820e20c2881c
giuelino <rgiulio59@gmail.com>
2026-07-09 17:34:00 +0200

    Aggiunta entity RepairReport per il manageBookingStatus

---
+++ src/main/java/it/dosti/justit/model/RepairReport.java
@@ -0,0 +1,28 @@
+package it.dosti.justit.model;
+
+public class RepairReport {
+
+    private String techNotes;
+    private double laborHours;
+    private double costHours;
+    private double partCosts;
+
+
+
+
+    public RepairReport(String techNotes, double laborHours, double costHours, double partCosts) {
+        this.techNotes = techNotes;
+        this.laborHours = laborHours;
+        this.costHours = costHours;
+        this.partCosts = partCosts;
+    }
+
+
+
+    public double calculateTotalCost() {
+
+        return  partCosts + laborHours * costHours;
+
+    }
+
+}
