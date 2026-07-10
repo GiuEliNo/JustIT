3e6e31e1d5c98d39f3eb8316868466c5df3359e9
giuelino <rgiulio59@gmail.com>
2026-07-10 17:16:53 +0200

    Modificata la factory del booking in modo da supportare il booking senza foreign key

--- src/main/java/it/dosti/justit/model/booking/BookingFactory.java
+++ src/main/java/it/dosti/justit/model/booking/BookingFactory.java
@@ -1,15 +1,17 @@
 package it.dosti.justit.model.booking;
 
 import it.dosti.justit.bean.BookingBean;
+import it.dosti.justit.model.Shop;
 import it.dosti.justit.model.TimeSlot;
+import it.dosti.justit.model.user.User;
 
 public class BookingFactory {
 
     private BookingFactory() {}
-    //TODO aggiungere differenziazione tra i due tipi di booking (a domicilio e in negozio)
-    public static Booking createBooking(BookingBean bookingBean) {
-        return new Booking.Builder(bookingBean.getUsername())
-                .shopId(bookingBean.getShopId())
+
+    public static Booking createBookingFromBean(BookingBean bookingBean, Shop shop, User user) {
+        return new Booking.Builder(user)
+                .shopEntity(shop)
                 .date(bookingBean.getDate())
                 .timeSlot((TimeSlot.valueOf(bookingBean.getTimeSlot())))
                 .description(bookingBean.getDescription())
